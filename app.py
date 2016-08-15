9df22ebf2acde7e17736492bbefbdd305af308ef
Ari Ramdial <ari.ramdial@gmail.com>
2016-08-14 23:20:43 -0400

    Add mailgun webhook

--- app.py
+++ app.py
@@ -32,25 +32,15 @@ def webhook():
 def processRequest(req):
     if req.get("result").get("action") != "rhobot-email":
         return {}
-    baseurl = "https://query.yahooapis.com/v1/public/yql?"
-    yql_query = makeYqlQuery(req)
-    if yql_query is None:
+    print req
+    email_query = send_simple_message(req)
+    if email_query is None:
         return {}
-    yql_url = baseurl + urllib.urlencode({'q': yql_query}) + "&format=json"
-    result = urllib.urlopen(yql_url).read()
-    data = json.loads(result)
+    data = json.loads(email_query)
     res = makeWebhookResult(data)
     return res
 
 
-def makeYqlQuery(req):
-    result = req.get("result")
-    parameters = result.get("parameters")
-    city = parameters.get("geo-city")
-    if city is None:
-        return None
-
-    return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
 
 def send_simple_message(req):
     result = req.get("result")
@@ -59,9 +49,9 @@ def send_simple_message(req):
     email = parameters.get("from_email")
     message = parameters.get("message")
     return requests.post(
-        "https://api.mailgun.net/v3/YOUR_DOMAIN_NAME/messages",
+        "https://api.mailgun.net/v3/sandboxee25071432c844e08c28e9438f9f8986.mailgun.org/messages",
         auth=("api", "key-bece1656953b819fbe56fc0e5f22a0d2"),
-        data={"from": <email>,
+        data={"from": email,
               "to": ["bar@example.com", "postmaster@sandboxee25071432c844e08c28e9438f9f8986.mailgun.org"],
               "subject": "Rho",
               "text": message})
